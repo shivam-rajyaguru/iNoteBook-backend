38aa02cab0c1fbe16db32e5e7288766c8d725081
shiva <guru_shiva1625@gmail.com>
2024-01-31 11:44:44 +0530

    add update and delete endpoints in notes

--- middleware/fetchuser.js
+++ middleware/fetchuser.js
@@ -2,14 +2,18 @@ const jwt = require("jsonwebtoken");
 const fetchuser = (req, res, next) => {
   const token = req.header("auth-token");
   if (!token) {
-    res.status(401).json({ error: "Please authenticate using a valid token" });
+    return res
+      .status(401)
+      .json({ error: "Please authenticate using a valid token" });
   }
   try {
     const data = jwt.verify(token, "ThisIsMySecretKey");
     // console.log(data);
     req.user = data;
   } catch (error) {
-    res.status(401).json({ error: "Please authenticate using a valid token" });
+    return res
+      .status(401)
+      .json({ error: "Please authenticate using a valid token" });
   }
   next();
 };
--- routes/notes.js
+++ routes/notes.js
@@ -41,4 +41,47 @@ router.post("/addnotes", fetchuser, async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
 });
+
+router.put("/updatenotes/:id", fetchuser, async (req, res) => {
+  const { title, description, tag } = req.body;
+  const newNote = {};
+  if (title) {
+    newNote.title = title;
+  }
+  if (description) {
+    newNote.description = description;
+  }
+  if (tag) {
+    newNote.tag = tag;
+  }
+
+  //first check if note is available or not
+  let note = await Note.findById(req.params.id);
+  if (!note) {
+    return res.status(404).send("Not found");
+  }
+  if (note.user.toString() !== req.user.id) {
+    return res.status(401).send("Not allowed");
+  }
+
+  note = await Note.findByIdAndUpdate(
+    req.params.id,
+    { $set: newNote },
+    { new: true }
+  );
+  res.status(200).json(note);
+});
+
+router.delete("/deletenote/:id", fetchuser, async (req, res) => {
+  let note = await Note.findById(req.params.id);
+  if (!note) {
+    return res.status(404).send("Not found");
+  }
+
+  if (note.user.toString() !== req.user.id) {
+    return res.status(401).send("Not allowed");
+  }
+  note = await Note.findByIdAndDelete(req.params.id);
+  res.status(200).json({ msg: "Succesfully deleted", user: note });
+});
 module.exports = router;
